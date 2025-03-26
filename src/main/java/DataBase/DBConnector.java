5de5f56d8ce3fd44c93734670d51f90da44e2d76
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-03-26 14:11:55 +0100

    krijimi i DBConnector sebashku me perfshirjen e postgreSQL ne pom.xml

---
+++ src/main/java/DataBase/DBConnector.java
@@ -0,0 +1,29 @@
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private static final String DB_URL = "jdbc:postgresql://localhost/knk_projekt";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "1212";
+
+    private static Connection connection = null;
+    public Connection getConnection() {
+        if(connection == null) {
+            try{
+                connection = DriverManager.getConnection(
+                        DB_URL,
+                        DB_USER,
+                        DB_PASSWORD
+                );
+            }catch(SQLException e){
+                return null;
+            }
+        }
+        return connection;
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,10 @@
-module com.example.knk_25 {
+module java {
     requires javafx.controls;
     requires javafx.fxml;
 
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.knk_25 to javafx.fxml;
     exports com.example.knk_25;
