0ca955a8e17110d525c841d3418c7b8f99fa6f40
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-04-26 21:52:48 +0200

    plotesimi i dto

--- src/main/java/Models/DTO/createEventDTO.java
+++ src/main/java/Models/DTO/createEventDTO.java
@@ -1,4 +1,74 @@
 package Models.DTO;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+//CREATE TABLE Event (
+  //      id SERIAL PRIMARY KEY,
+     //   event_name VARCHAR(255) NOT NULL,
+//organizer_name VARCHAR(255) NOT NULL,
+//event_date DATE NOT NULL,
+//event_time TIME NOT NULL,
+//room_id INT REFERENCES Room(id) ON DELETE SET NULL,
+//description TEXT
+//);
 public class createEventDTO {
+    private String event_name;
+    private String organizer_name;
+    private LocalDate event_date;
+    private LocalTime event_time;
+    private Integer room_id;
+    private String description;
+
+    public createEventDTO(String event_name, String organizer_name, LocalDate event_date, LocalTime event_time, int room_id, String description){
+        this.event_name = event_name;
+        this.organizer_name = organizer_name;
+        this.event_date = event_date;
+        this.event_time = event_time;
+        this.room_id = room_id;
+        this.description = description;
+    }
+    public String getEvent_name() {
+        return event_name;
+    }
+
+    public void setEvent_name(String event_name) {
+        this.event_name = event_name;
+    }
+    public String getOrganizer_name() {
+        return organizer_name;
+    }
+
+    public void setOrganizer_name(String organizer_name) {
+        this.organizer_name = organizer_name;
+    }
+    public LocalDate getEvent_date() {
+        return event_date;
+    }
+
+    public void setEvent_date(LocalDate event_date) {
+        this.event_date = event_date;
+    }
+    public LocalTime getEvent_time() {
+        return event_time;
+    }
+
+    public void setEvent_time(LocalTime event_time) {
+        this.event_time = event_time;
+    }
+    public int getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(int room_id) {
+        this.room_id = room_id;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
