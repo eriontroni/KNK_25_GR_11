e498c71e4fb9e079e7fe2007c7661b12802f4164
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-26 20:48:45 +0200

    fillimi i krijjimit te disa repositorys dhe perfundimi i DTO-s

--- src/main/java/Models/DTO/CreateNotificationDTO.java
+++ src/main/java/Models/DTO/CreateNotificationDTO.java
@@ -1,4 +1,55 @@
 package Models.DTO;
 
+import java.time.LocalDateTime;
+//
+//CREATE TABLE RoomImage (
+//        id SERIAL PRIMARY KEY,
+//        room_id INT REFERENCES Room(id) ON DELETE CASCADE,
+//image_url TEXT NOT NULL,
+//uploaded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//);
 public class CreateNotificationDTO {
+    private int id;
+    private int room_id;
+    private String image_url;
+    LocalDateTime uploaded_at;
+
+    public CreateNotificationDTO(int id, int room_id, String image_url, LocalDateTime uploaded_at) {
+        this.id = id;
+        this.room_id = room_id;
+        this.image_url = image_url;
+        this.uploaded_at = uploaded_at;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(int room_id) {
+        this.room_id = room_id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public LocalDateTime getUploaded_at() {
+        return uploaded_at;
+    }
+
+    public void setUploaded_at(LocalDateTime uploaded_at) {
+        this.uploaded_at = uploaded_at;
+    }
 }
--- src/main/java/repository/RoomImageRepository.java
+++ src/main/java/repository/RoomImageRepository.java
@@ -1,4 +1,29 @@
 package repository;
 
-public class RoomImageRepository {
+import Models.DTO.CreateRoomImageDTO;
+import Models.DTO.UpdateRoomImageDTO;
+import Models.RoomImage;
+
+import java.sql.ResultSet;
+
+public class RoomImageRepository extends BaseRepository<RoomImage, CreateRoomImageDTO,UpdateRoomImageDTO> {
+
+    public RoomImageRepository(String tableName) {
+        super("RoomImage");
+    }
+
+    @Override
+    RoomImage fromResutlSet(ResultSet res) {
+        return null;
+    }
+
+    @Override
+    RoomImage create(CreateRoomImageDTO createRoomImageDTO) {
+        return null;
+    }
+
+    @Override
+    RoomImage udpate(UpdateRoomImageDTO updateRoomImageDTO) {
+        return null;
+    }
 }
