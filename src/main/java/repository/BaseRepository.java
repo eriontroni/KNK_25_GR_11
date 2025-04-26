446639c1e031c111f8154441132c4571bbc523ec
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-26 20:29:32 +0200

    Krijimi i BaseRepositoryt
    poashtu inicializimi i RoomImageRepositoryt

---
+++ src/main/java/repository/BaseRepository.java
@@ -0,0 +1,71 @@
+package repository;
+
+import DataBase.DBConnector;
+import Models.*;
+import java.sql.*;
+import java.util.ArrayList;
+// cdo komunikim me baze te te dhenave, behet permes repositories
+
+abstract class BaseRepository<Model, CreateModelDto, UpdateModelDto> {
+    protected Connection connection;
+    private String tableName;
+
+    public BaseRepository(String tableName) {
+        this.connection = DBConnector.getConnection();
+        this.tableName = tableName;
+    }
+
+
+    abstract Model fromResutlSet(ResultSet res);
+
+    public Model getById(int id){
+        String query = "SELECT * FROM " + this.tableName + " WHERE id = ?";
+        try{
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet res = statement.executeQuery(query);
+            if(res.next()){
+                return this.fromResutlSet(res);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList<Model> getAll(){
+        ArrayList<Model> models = new ArrayList<>();
+        String query = "SELECT * FROM " + this.tableName;
+        try{
+            Statement statement = this.connection.createStatement();
+            ResultSet res = statement.executeQuery(query);
+            while(res.next()){
+                models.add(this.fromResutlSet(res));
+            }
+
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        return models;
+    }
+
+    abstract Model create(CreateModelDto createModelDto);
+
+    abstract Model udpate(UpdateModelDto updateModelDto);
+
+//    abstract boolean delete(int id);
+
+
+    public boolean delete(int id){
+        String query = "DELETE FROM ??? WHERE ID = ?";
+        try{
+            PreparedStatement pstm = this.connection.prepareStatement(query);
+            pstm.setInt(1, id);
+            return pstm.executeUpdate() == 1;
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
