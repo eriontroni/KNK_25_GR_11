aea91209b9b42586dfb60956f96b777e17b48261
eriontroni <142906809+eriontroni@users.noreply.github.com>
2025-04-26 17:21:27 +0200

    Rregullimi i LocalDateTime dhe objektit ReservationHistory

--- src/main/java/Models/ReservationHistory.java
+++ src/main/java/Models/ReservationHistory.java
@@ -1,4 +1,62 @@
 package Models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ReservationHistory {
+    private int id;
+    private int reservationId;
+    private int customerId;
+    private LocalDateTime changeDate;
+    private String oldStatus;
+    private String newStatus;
+
+    private ReservationHistory(int id, int reservationId, int customerId, LocalDateTime changeDate, String oldStatus, String newStatus) {
+        this.id = id;
+        this.reservationId = reservationId;
+        this.customerId = customerId;
+        this.changeDate = changeDate;
+        this.oldStatus = oldStatus;
+        this.newStatus = newStatus;
+    }
+
+    public static ReservationHistory getInstance(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        int reservationId = resultSet.getInt("reservation_id");
+        int customerId = resultSet.getInt("customer_id");
+
+        String changeDateString = resultSet.getString("change_date");
+        LocalDateTime changeDate = LocalDateTime.parse(changeDateString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        String oldStatus = resultSet.getString("old_status");
+        String newStatus = resultSet.getString("new_status");
+
+        return new ReservationHistory(id, reservationId, customerId, changeDate, oldStatus, newStatus);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public LocalDateTime getChangeDate() {
+        return changeDate;
+    }
+
+    public String getOldStatus() {
+        return oldStatus;
+    }
+
+    public String getNewStatus() {
+        return newStatus;
+    }
 }
--- src/main/java/Models/RoomImage.java
+++ src/main/java/Models/RoomImage.java
@@ -7,11 +7,11 @@ import java.time.format.DateTimeFormatter;
 
 public class RoomImage {
     private int id;
-    int roomID; // referencon idn e room;
-    String imgURL; //path per deri tek image
-    String uploadedAt;
+    private int roomID; // referencon idn e room;
+    private String imgURL; //path per deri tek image
+    private LocalDateTime  uploadedAt;
 
-    private RoomImage(int id, int roomID, String imgURL, String uploadedAt) {
+    private RoomImage(int id, int roomID, String imgURL, LocalDateTime uploadedAt) {
         this.id = id;
         this.roomID = roomID;
         this.imgURL = imgURL;
@@ -21,7 +21,8 @@ public class RoomImage {
         int id = resultset.getInt("id");
         int roomID = resultset.getInt("room_id");
         String imgURL = resultset.getString("img_url");
-        String uploadedAt = resultset.getString("uploaded_at");
+        String uploadedAtString = resultset.getString("uploaded_at");
+        LocalDateTime uploadedAt = LocalDateTime.parse(uploadedAtString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         return new RoomImage(id, roomID, imgURL, uploadedAt);
     }
 
@@ -37,7 +38,7 @@ public class RoomImage {
         return imgURL;
     }
 
-    public String getUploadedAt() {
+    public LocalDateTime getUploadedAt() {
         return uploadedAt;
     }
 }
