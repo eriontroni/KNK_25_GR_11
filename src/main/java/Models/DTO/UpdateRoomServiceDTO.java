32bfe2248ee0c65d63541d533408d1bd14c61cb3
eraberisha <era.berisha5@student.uni-pr.edu>
2025-04-27 18:45:53 +0200

    Permiresim

--- src/main/java/Models/DTO/UpdateRoomServiceDTO.java
+++ src/main/java/Models/DTO/UpdateRoomServiceDTO.java
@@ -1,12 +1,18 @@
 package Models.DTO;
 
 public class UpdateRoomServiceDTO {
+    private int id;
     private String serviceName;
     private double price;
 
-    public UpdateRoomServiceDTO(String serviceName, double price){
+    public UpdateRoomServiceDTO(int id, String serviceName, double price){
+        this.id=id;
         this.serviceName=serviceName;
         this.price=price;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getServiceName() {
