da052287acaec02f815b9304dfed9069457de17c
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-04-28 16:09:24 +0200

    plotesim i update

--- src/main/java/Models/DTO/updateRoomTypeDTO.java
+++ src/main/java/Models/DTO/updateRoomTypeDTO.java
@@ -1,4 +1,57 @@
 package Models.DTO;
 
+//CREATE TABLE RoomType (
+//      id SERIAL PRIMARY KEY,
+//    name VARCHAR(100) NOT NULL,
+//description TEXT,
+//capacity INT CHECK (capacity > 0),
+//price_per_night DECIMAL(10,2) CHECK (price_per_night >= 0),
+//created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+//updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//);
+
 public class updateRoomTypeDTO {
+    private String name;
+    private String description;
+    private int capacity;
+    private float price_per_night;
+
+    public updateRoomTypeDTO(String name, String description, int capacity, float price_per_night) {
+        this.name = name;//Mund të ndryshosh emrin e tipit të dhomës (p.sh. nga "Deluxe" në "Super Deluxe").
+        this.description = description;//Mund të ndryshosh përshkrimin nëse ndryshon informacioni.
+        this.capacity = capacity;//	Mund të ndryshosh kapacitetin (p.sh. nga 2 persona në 3 persona).
+        this.price_per_night = price_per_night;//	Mund të ndryshosh çmimin për natë në varësi të ofertave, sezonit etj.
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public float getPrice_per_night() {
+        return price_per_night;
+    }
+
+    public void setPrice_per_night(float price_per_night) {
+        this.price_per_night = price_per_night;
+    }
 }
+
