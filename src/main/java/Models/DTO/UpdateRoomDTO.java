7b93ca761f64adba7e2350a752778c32b8bc3f67
natyrabajgora <natyrabajgora11@gmail.com>
2025-04-29 14:48:43 +0200

    Update Maintenance, Room dhe Reservation

--- src/main/java/Models/DTO/UpdateRoomDTO.java
+++ src/main/java/Models/DTO/UpdateRoomDTO.java
@@ -1,4 +1,31 @@
 package Models.DTO;
 
 public class UpdateRoomDTO {
+    private int id;
+    private String roomNumber;
+    private int typeId;
+    private boolean isAvailable;
+
+    public UpdateRoomDTO(int id, String roomNumber, int typeId, boolean isAvailable) {
+        this.id = id;
+        this.roomNumber = roomNumber;
+        this.typeId = typeId;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
