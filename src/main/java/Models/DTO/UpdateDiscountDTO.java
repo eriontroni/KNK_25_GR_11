ccdabffe4bdb0b99cb23547e4059019d7725add0
vesahadergjonaj <vesa.hadergjonaj@gmail.com>
2025-04-29 13:47:24 +0200

    krijimi i klasave update

--- src/main/java/Models/DTO/UpdateDiscountDTO.java
+++ src/main/java/Models/DTO/UpdateDiscountDTO.java
@@ -1,4 +1,52 @@
 package Models.DTO;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class UpdateDiscountDTO {
+    private String description;
+    private BigDecimal percentage;
+    private Date valid_from;
+    private Date valid_to;
+
+    public UpdateDiscountDTO() {}
+
+    public UpdateDiscountDTO(String description, BigDecimal percentage, Date valid_from, Date valid_to) {
+        this.description = description;
+        this.percentage = percentage;
+        this.valid_from = valid_from;
+        this.valid_to = valid_to;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+
+    public Date getValid_from() {
+        return valid_from;
+    }
+
+    public void setValid_from(Date valid_from) {
+        this.valid_from = valid_from;
+    }
+
+    public Date getValid_to() {
+        return valid_to;
+    }
+
+    public void setValid_to(Date valid_to) {
+        this.valid_to = valid_to;
+    }
 }
--- src/main/java/Models/DTO/UpdateUsersDTO.java
+++ src/main/java/Models/DTO/UpdateUsersDTO.java
@@ -1,4 +1,27 @@
 package Models.DTO;
 
 public class UpdateUsersDTO {
+    private String username;
+    private String email;
+    private String role;
+
+    public UpdateUsersDTO() {}
+
+    public UpdateUsersDTO(String username, String email, String role) {
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getRole() { return role; }
+
+    public void setRole(String role) { this.role = role; }
 }
