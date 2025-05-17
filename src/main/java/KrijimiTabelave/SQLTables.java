866eee69d360dfaf43ae0c67ee549f49c26f86e9
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-05-17 22:07:20 +0200

    Update SQLTables.java

--- src/main/java/KrijimiTabelave/SQLTables.java
+++ src/main/java/KrijimiTabelave/SQLTables.java
@@ -52,7 +52,7 @@ CREATE TABLE Users (
     username VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    salted_hash TEXT NOT NULL,
+    salted_hash TEXT NOT NULL
 );
 
 -- 5. Employee -Elona
