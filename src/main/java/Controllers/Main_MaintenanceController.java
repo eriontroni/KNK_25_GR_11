30cbcfa08887198985b752d2055958e91c6fa829
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-05-18 17:10:07 +0200

    krjim i kontrollerit

---
+++ src/main/java/Controllers/Main_MaintenanceController.java
@@ -0,0 +1,37 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main_MaintenanceController {
+
+    @FXML
+    private void handleMirembajtje(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/views/maintenance.fxml"));
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Mirembajtje");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handlePastrim(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/views/cleaningschedule.fxml"));
+            Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Pastrim");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
