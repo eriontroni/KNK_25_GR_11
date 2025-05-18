c511268320bf702fe5afe9cd29913e24de6d494e
vesahadergjonaj <vesa.hadergjonaj@gmail.com>
2025-05-18 15:56:24 +0200

    permirsime

---
+++ src/main/java/App/vesa.java
@@ -0,0 +1,17 @@
+package App;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class vesa extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/home.fxml"));
+        Scene scene = new Scene(loader.load(), 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("Hotel Offers");
+        stage.show();
+    }
+}
