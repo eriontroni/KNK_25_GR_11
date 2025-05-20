5c824f113d72bcc20ce21903ae999dfdc64ee0e3
vesahadergjonaj <vesa.hadergjonaj@gmail.com>
2025-05-20 02:01:08 +0200

    permirsimi i kontrollerit per te mundsuar qasjen ne te dhena

--- src/main/java/Controllers/OffersController.java
+++ src/main/java/Controllers/OffersController.java
@@ -6,16 +6,35 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import Models.Offer;
+import repository.OfferRepository;
+
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OffersController {
 
+    @FXML private TableView<Offer> offersTable;
+    @FXML private TableColumn<Offer, String> titleCol;
+    @FXML private TableColumn<Offer, String> descriptionCol;
+    @FXML private TableColumn<Offer, Double> discountCol;
+    @FXML private TableColumn<Offer, String> startDateCol;
+    @FXML private TableColumn<Offer, String> endDateCol;
     @FXML private Button btnBackToHome;
-    @FXML private Label lblTitle;
     @FXML private Label currentDateLabel;
+    @FXML private Label lblTitle;
+    private Connection connection;
 
 
     @FXML
@@ -26,6 +45,44 @@ public class OffersController {
 
         // Back button action
         btnBackToHome.setOnAction(e -> goBackToHome());
+
+        // Setup columns
+        titleCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        descriptionCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+        discountCol.setCellValueFactory(new PropertyValueFactory<>("discountPercentage"));
+        startDateCol.setCellValueFactory(new PropertyValueFactory<>("startDate"));
+        endDateCol.setCellValueFactory(new PropertyValueFactory<>("endDate"));
+
+        // Populate the table
+        loadOffers();
+    }
+    private void loadOffers() {
+        OfferRepository offerRepository = new OfferRepository("Offer");
+        var offers = offerRepository.getAll(); // kjo do kthejë listën e ofertave
+
+        if (offers != null) {
+            offersTable.getItems().setAll(offers);
+        }
+    }
+    public List<Offer> getAll() {
+        List<Offer> offers = new ArrayList<>();
+        String query = "SELECT * FROM Offer";
+
+        try {
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            ResultSet res = statement.executeQuery();
+
+            while (res.next()) {
+                Offer offer = Offer.getInstance(res);;
+                if (offer != null) {
+                    offers.add(offer);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return offers;
     }
 
     private void goBackToHome() {
