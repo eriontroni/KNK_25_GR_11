eeb4b60c0473a2bdafdd93fd0ea0e2be7a9f1879
leontroni <leontroni69@gmail.com>
2025-05-21 23:02:23 +0200

    Permisimi final i ReceptionDashboard

--- src/main/java/Controllers/ReceptionDashboardController.java
+++ src/main/java/Controllers/ReceptionDashboardController.java
@@ -1,32 +1,39 @@
 package Controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
-import javafx.scene.Node;
 import java.io.IOException;
 
 public class ReceptionDashboardController {
 
     @FXML
-    private void handlePerRole(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/ChartPerRole.fxml"));
-        Scene scene = new Scene(loader.load());
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.setTitle("Numri i përdoruesve për çdo rol");
-        stage.show();
+    private void handlePerRole() {
+        openWindow("/views/ChartPerRole.fxml", "Numri i përdoruesve për çdo rol");
     }
 
     @FXML
-    private void handlePerDay(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/ChartPerDay.fxml"));
-        Scene scene = new Scene(loader.load());
-        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        stage.setScene(scene);
-        stage.setTitle("Numri i rezervimeve për çdo ditë");
-        stage.show();
+    private void handleCreateReservation() {
+        openWindow("/views/Reservations.fxml", "Krijo Rezervim");
+    }
+
+    @FXML
+    private void handleAddStaff() {
+        openWindow("/views/signupStafi.fxml", "Shto Staf");
+    }
+
+    private void openWindow(String fxmlPath, String title) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            Stage newStage = new Stage();
+            newStage.setTitle(title);
+            newStage.setScene(new Scene(root));
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
